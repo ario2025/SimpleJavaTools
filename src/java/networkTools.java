892646d1bf1f6ab0df2994e5a3bc69cb43cd7f40
Aristóteles Machado <aristoteles.machado@vilt-group.com>
2019-09-20 10:45:34 -0300

    First update.

--- src/java/networkTools.java
+++ src/java/networkTools.java
@@ -1,4 +1,4 @@
-import java.io.DataOutputStream;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -12,6 +12,7 @@ public class networkTools {
             con.setInstanceFollowRedirects(true);
             con.setConnectTimeout(1000);
             con.setReadTimeout(1000);
+            con.setRequestProperty("User-Agent", "Java Application");
             con.setRequestProperty("Content-Type", "application/json");
             con.setRequestProperty("Content-", "application/json");
             con.setDoOutput(true);
@@ -19,7 +20,9 @@ public class networkTools {
             out.writeBytes("");
             out.flush();
             out.close();
-            System.out.println("Status: " + con.getResponseCode() + ": " + con.getResponseMessage());
+            String httpRetorno = convertStreamToString( con.getInputStream() );
+            System.out.println("Status: " + con.getResponseCode() + ": " + con.getResponseMessage() + "\n\n");
+            return httpRetorno;
         } catch (Exception e) {
             System.out.println(e);
         }
@@ -36,11 +39,34 @@ public class networkTools {
             con.setReadTimeout(1000);
             con.setRequestProperty("Content-Type", "application/json");
             con.setDoOutput(true);
-            System.out.println("Status: " + con.getResponseCode() + ": " + con.getResponseMessage());
+            String httpRetorno = convertStreamToString( con.getInputStream() );
+            System.out.println("Status: " + con.getResponseCode() + ": " + con.getResponseMessage() + "\n\n");
+            return httpRetorno;
         } catch (Exception e) {
             System.out.println(e);
         }
         return "Ok";
+    }
+
+
+    public static String convertStreamToString(InputStream is) throws IOException {
+
+        if (is != null) {
+            StringBuilder sb = new StringBuilder();
+            String line;
+            try {
+                BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(is, "UTF-8"));
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line).append("\n");
+                }
+            } finally {
+                is.close();
+            }
+            return sb.toString();
+        } else {
+            return "";
+        }
     }
 
 }
