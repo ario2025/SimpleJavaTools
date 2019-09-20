88ae5d6f448fbcd48afd56da752182ba120670bd
Aristóteles Machado <aristoteles.machado@vilt-group.com>
2019-09-20 15:50:34 -0300

    Starters

--- Start.bat
+++ Start.bat
@@ -1,23 +1,10 @@
 @ECHO OFF
-
-MD Target
-
+RMDIR /S/Q TARGET
+MD target
 CLS
-
 ECHO Minimalism Java Tools.
-
-REM javac -d target -cp "./;" src/java/*.java
-
 javac -d target src/java/*.java
-
-CD Target
-
+CD target
 java start
-
-REM jar cfm start.jar ../manifest.mf start start.class
-
-REM java -jar start.jar
-
 PAUSE
-
 EXIT
--- StartJAR.bat
+++ StartJAR.bat
@@ -1,10 +1,12 @@
-REM @ECHO OFF
-MD Target
+@ECHO OFF
+RMDIR /S/Q TARGET
+MD target
 CLS
 ECHO Minimalism Java Tools.
 javac -d target src/java/*.java
 CD Target
 jar cfm start.jar ../manifest.mf *.class
+del *.class
 java -jar start.jar
 PAUSE
 EXIT
